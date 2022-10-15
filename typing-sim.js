9b78389151ee45ee161a851d49371c4cc3f266ae
Jeremy Aubrey <jeremyaubrey@gmail.com>
2022-10-14 20:43:36 -0500

    added blinking cursor feature

--- typing-sim.js
+++ typing-sim.js
@@ -6,20 +6,33 @@ const typeOut = (phrase, target) => {
         setTimeout(() => {
             if(phrase[i] === " ") {
                 target.innerHTML += "&nbsp;";
+            } else {
+                target.innerText = target.innerText + phrase[i];
             }
-            target.innerText = target.innerText + phrase[i];
+
+            if(i === phrase.length - 1) {
+                toggleCursor(target);
+            }
         }, randDelay(i))
-        // target.innerText = target.innerText + phrase[i];
     }
 }
 
 // Random delay function
 const randDelay = (multiplier) => {
-    let delay = (Math.random() + multiplier) * 75;
-    console.log(delay);
+    let delay = (Math.random() + multiplier) * 130;
     return delay;
 }
 
+// Displays a blinking cursor
+const toggleCursor = (target) => {
+    setInterval(() => {
+        let lastCharacterIndex = target.innerText.length - 1;
+        target.innerText[lastCharacterIndex] === "_" ? 
+        target.innerText = target.innerText.slice(0, -1) :
+        target.innerText += "_";
+    }, 500)
+}
+
 // Test
-let phrase = "Hello World, finally this thing is working...";
+let phrase = "Hello World, my name is Jeremy";
 typeOut(phrase, testDiv);
